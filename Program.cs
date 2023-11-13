42fbb81a9e1d6963b7b46749078758e8f15b6eb7
Faith nkatha <faithnkatha160@gmail.com>
2023-11-14 01:04:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class QuestionOne
+{
+    static void Main()
+    {
+        char letter;
+
+
+        Console.WriteLine("Enter a number ,letter or special character");
+        letter=Convert.ToChar(Console.ReadLine());
+        if (char.IsDigit(letter))
+            Console.WriteLine("You have entered a digit");
+        else if (char.IsLetter(letter))
+            Console.WriteLine("You have entered a letter");
+        else
+            Console.WriteLine("You have entered a special character");
+    }
+}
